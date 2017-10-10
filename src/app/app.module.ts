e3b0a4e7a79531c716e78172560551ad9436b0f6
Rafael dos Santos Calderani <rafael.calderani@gmail.com>
2017-10-09 22:10:23 -0300

    final commit?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { RoutingModule } from './routing/routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MomentModule} from 'angular2-moment';
+import 'moment/locale/pt-br';
 
 /* Serviços */
 import { DespesaService } from './despesa.service';
@@ -15,12 +17,14 @@ import { AddComponent } from './add/add.component';
 
 /* LocalStorage */
 import { LocalStorageModule } from 'angular-2-local-storage';
+import { DecimalBrPipe } from './decimal-br.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    AddComponent
+    AddComponent,
+    DecimalBrPipe
   ],
   imports: [
     BrowserModule,
@@ -30,7 +34,8 @@ import { LocalStorageModule } from 'angular-2-local-storage';
     LocalStorageModule.withConfig({
       prefix: 'despesasApp',
       storageType: 'localStorage'
-    })
+    }),
+    MomentModule
   ],
   providers: [DespesaService],
   bootstrap: [AppComponent]
---
+++ src/app/decimal-br.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'decimalBr'
+})
+export class DecimalBrPipe implements PipeTransform {
+  transform(value: number): string {
+    return value.toString().replace(",", "&").replace(".",",").replace("&",".");
+  }
+}
